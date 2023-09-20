a85bfc946e16fdd29744e1b92404fb31948c1154
Wesley Alves <wesley.info07@gmail.com>
2023-09-20 17:05:29 -0300

    Anotações adapter.

--- Design Patterns/02 - Structural/2.1 - Adapter/ExecucaoAdapter.cs
+++ Design Patterns/02 - Structural/2.1 - Adapter/ExecucaoAdapter.cs
@@ -7,6 +7,7 @@
             var pagamentoLogPadrao = new TransacaoService(new Logger());
             pagamentoLogPadrao.RealizarTransacao();
 
+            //Adaptador(LogAdapter), recebe uma injeção de dependência da adaptação(LogNetMasterService).
             var pagamentoLogCustom = new TransacaoService(new LogAdapter(new LogNetMasterService()));
             pagamentoLogCustom.RealizarTransacao();
         }
